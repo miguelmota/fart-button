1979d5f091432c3b499c611efca71ff30a9f2888
Miguel Mota <miguelmota2@gmail.com>
2018-05-10 14:22:26 -0700

    add bin

--- index.js
+++ index.js
@@ -7,60 +7,63 @@ var BigRedButton = require('big-red-button');
 var request = require('request');
 var Q = require('q');
 
-var mediaFiles = [];
+function start() {
+  var mediaFiles = [];
+  var mediaDir = 'media/';
 
-var mediaDir = 'media/';
-
-fs.readdir([__dirname, '/', mediaDir].join(''), function(err, files) {
-  mediaFiles = _.map(_.filter(files, function(f, i) {
-    return /mp3/.test(f);
-  }), function(f, i) {
-    return [mediaDir, f].join('');
+  fs.readdir([__dirname, '/', mediaDir].join(''), function(err, files) {
+    mediaFiles = _.map(_.filter(files, function(f, i) {
+      return /mp3/.test(f);
+    }), function(f, i) {
+      return [mediaDir, f].join('');
+    });
   });
-});
 
-function randomMediaFile() {
-  return mediaFiles[_.random(0,mediaFiles.length)];
+  function randomMediaFile() {
+    return mediaFiles[_.random(0,mediaFiles.length)];
+  }
+
+  function getJoke() {
+    var deferred = Q.defer();
+
+    request('http://api.yomomma.info/', function(err, res, body) {
+        try {
+          var joke = JSON.parse(body.match(/({"j.*(.*).*})/gi)[0]).joke;
+          deferred.resolve(joke);
+        } catch(e) {
+          deferred.reject(e);
+        }
+    });
+
+    return deferred.promise;
+  }
+
+  var bigRedButtons = [];
+
+  for (var i = 0; i < BigRedButton.deviceCount(); i++) {
+    console.log('opening BigRedButton', i);
+
+    bigRedButtons.push(new BigRedButton.BigRedButton(i));
+
+    bigRedButtons[i].on('buttonPressed', function () {
+      console.log('Button pressed');
+
+      exec(['afplay', randomMediaFile()].join(' '), function(error, stdout, stderr) {});
+    });
+
+    bigRedButtons[i].on('buttonReleased', function () {
+      console.log('Button released');
+    });
+
+    bigRedButtons[i].on('lidRaised', function () {
+      console.log('Lid raised');
+    });
+
+    bigRedButtons[i].on('lidClosed', function () {
+      console.log('Lid closed');
+    });
+
+  }
 }
 
-function getJoke() {
-  var deferred = Q.defer();
-
-  request('http://api.yomomma.info/', function(err, res, body) {
-      try {
-        var joke = JSON.parse(body.match(/({"j.*(.*).*})/gi)[0]).joke;
-        deferred.resolve(joke);
-      } catch(e) {
-        deferred.reject(e);
-      }
-  });
-
-  return deferred.promise;
-}
-
-var bigRedButtons = [];
-
-for (var i = 0; i < BigRedButton.deviceCount(); i++) {
-  console.log('opening BigRedButton', i);
-
-  bigRedButtons.push(new BigRedButton.BigRedButton(i));
-
-  bigRedButtons[i].on('buttonPressed', function () {
-    console.log('Button pressed');
-
-    exec(['afplay', randomMediaFile()].join(' '), function(error, stdout, stderr) {});
-  });
-
-  bigRedButtons[i].on('buttonReleased', function () {
-    console.log('Button released');
-  });
-
-  bigRedButtons[i].on('lidRaised', function () {
-    console.log('Lid raised');
-  });
-
-  bigRedButtons[i].on('lidClosed', function () {
-    console.log('Lid closed');
-  });
-
-}
+module.exports = {start}
